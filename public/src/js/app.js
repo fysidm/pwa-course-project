1021b7ef1beb817b9e5252cd8347ae864eba97c9
GeorgeWu <fysidm@gmail.com>
2022-01-05 23:02:02 +0800

    Controlling install prompt

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,6 +1,16 @@
+let deferredPrompt;
+
 if ('serviceWorker' in navigator) {
   // Register service worker
   navigator.serviceWorker.register('/sw.js').then(() => {
     console.log('Service worker registered!');
   });
 }
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  console.log('beforeinstallprompt was fired');
+  // Won't show the banner
+  e.preventDefault();
+  deferredPrompt = e;
+  return false;
+});
